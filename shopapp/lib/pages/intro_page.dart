5615450a76e0440b6c35ac30a69f57c556ab0912
amtech1 <amanhussain.ash@gmail.com>
2023-12-31 12:15:00 +0500

    my second commit intro page in progress

---
+++ shopapp/lib/pages/intro_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.shopping_cart,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            const Text(
+              "The One Shop",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Premium Luxury  Products",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
